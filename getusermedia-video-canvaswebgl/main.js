b26660054c6e4d193d6dbccca340d67b8a39adc4
adcimon <37084114+adcimon@users.noreply.github.com>
2020-10-17 20:38:10 +0200

    Minor changes

--- getusermedia-video-canvaswebgl/main.js
+++ getusermedia-video-canvaswebgl/main.js
@@ -1,32 +1,33 @@
 var video, canvas, gl;
-
 var vertexBuffer, indexBuffer, textureCoordinateBuffer;
-
-var texture, program;
+var texture, unit = 0;
+var program;
 
 var vertexSource =
 `
-attribute vec3 aPosition;
-attribute vec2 aTextureCoordinate;
+attribute vec3 a_position;
+attribute vec2 a_texcoord;
 
-varying highp vec2 vTextureCoordinate;
+varying vec2 v_texcoord;
 
 void main()
 {
-	gl_Position = vec4(aPosition, 1.0);
-	vTextureCoordinate = aTextureCoordinate;
+	gl_Position = vec4(a_position, 1.0);
+	v_texcoord = a_texcoord;
 }
 `;
 
 var fragmentSource =
 `
-uniform sampler2D uFrame;
+precision mediump float;
 
-varying highp vec2 vTextureCoordinate;
+varying vec2 v_texcoord;
+
+uniform sampler2D u_frame;
 
 void main()
 {
-	gl_FragColor = texture2D(uFrame, vTextureCoordinate);
+	gl_FragColor = texture2D(u_frame, v_texcoord);
 }
 `;
 
@@ -44,29 +45,27 @@ function main()
 	canvas = document.getElementById("canvas");
 	gl = canvas.getContext("webgl");
 
-	init();
+	initialize();
 
 	video.addEventListener("canplay", onCanPlay);
 
 	window.navigator.mediaDevices.getUserMedia(constraints).then(onGetUserMediaSuccess).catch(onGetUserMediaError);
 }
 
-function init()
+function initialize()
 {
 	createBuffers();
 	createTexture();
 	createProgram();
+
+	useProgram();
+	bindBuffers();
+	setUniforms();
 }
 
 function render()
 {
-	gl.useProgram(program);
-
-	bindBuffers();
-
 	updateTexture();
-
-	bindTexture();
 
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 	gl.viewport(0, 0, canvas.width, canvas.height);
@@ -78,21 +77,21 @@ function render()
 function createBuffers()
 {
 	// Vertex buffer.
-	var vertices = [1, 1, 0, 1, -1, 0, -1, -1, 0, -1, 1, 0];
+	let vertices = [1, 1, 0, 1, -1, 0, -1, -1, 0, -1, 1, 0];
 	vertexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 	// Index buffer.
-	var indices = [0, 1, 2, 0, 2, 3];
+	let indices = [0, 1, 2, 0, 2, 3];
 	indexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
 	// Texture coordinate buffer.
-	var textureCoordinates = [1, 0, 1, 1, 0, 1, 0, 0];
+	let textureCoordinates = [1, 0, 1, 1, 0, 1, 0, 0];
 	textureCoordinateBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
@@ -102,12 +101,12 @@ function createBuffers()
 function bindBuffers()
 {
 	// Vertex buffer.
-	var positionAttribute = gl.getAttribLocation(program, "aPosition");
-	var size = 3;
-	var type = gl.FLOAT;
-	var normalized = false;
-	var stride = 0;
-	var offset = 0;
+	let positionAttribute = gl.getAttribLocation(program, "a_position");
+	let size = 3;
+	let type = gl.FLOAT;
+	let normalized = false;
+	let stride = 0;
+	let offset = 0;
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 	gl.vertexAttribPointer(positionAttribute, size, type, normalized, stride, offset);
 	gl.enableVertexAttribArray(positionAttribute);
@@ -116,12 +115,12 @@ function bindBuffers()
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
 	// Texture coordinate buffer.
-	var textureCoordinateAttribute = gl.getAttribLocation(program, "aTextureCoordinate");
-	var size = 2;
-	var type = gl.FLOAT;
-	var normalized = false;
-	var stride = 0;
-	var offset = 0;
+	let textureCoordinateAttribute = gl.getAttribLocation(program, "a_texcoord");
+	size = 2;
+	type = gl.FLOAT;
+	normalized = false;
+	stride = 0;
+	offset = 0;
 	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
 	gl.vertexAttribPointer(textureCoordinateAttribute, size, type, normalized, stride, offset);
 	gl.enableVertexAttribArray(textureCoordinateAttribute);
@@ -131,57 +130,49 @@ function createTexture()
 {
 	texture = gl.createTexture();
 
+	// Active the texture unit.
+	gl.activeTexture(gl.TEXTURE0 + unit);
+
+	// Bind the texture to the texture unit.
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 
-	var level = 0;
-	var internalFormat = gl.RGBA;
-	var width = 1;
-	var height = 1;
-	var border = 0;
-	var format = gl.RGBA;
-	var type = gl.UNSIGNED_BYTE;
-	var pixels = new Uint8Array([0, 0, 0, 255]);
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, pixels);
-
-	// Turn off mips and set wrapping to clamp to edge so it will work regardless of the dimensions of the video.
+	// Turn off mip maps and set wrapping to clamp to edge so it will work regardless of the dimensions of the video.
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+	let target = gl.TEXTURE_2D;
+	let level = 0;
+	let internalFormat = gl.RGBA;
+	let width = 1;
+	let height = 1;
+	let border = 0;
+	let format = gl.RGBA;
+	let type = gl.UNSIGNED_BYTE;
+	let pixels = new Uint8Array([0, 0, 0, 255]);
+	gl.texImage2D(target, level, internalFormat, width, height, border, format, type, pixels);
 }
 
 function updateTexture()
 {
-	var level = 0;
-	var internalFormat = gl.RGBA;
-	var format = gl.RGBA;
-	var type = gl.UNSIGNED_BYTE;
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, format, type, video);
-}
-
-function bindTexture()
-{
-	var frameUniform = gl.getUniformLocation(program, "uFrame");
-
-	// Active texture unit 0.
-	gl.activeTexture(gl.TEXTURE0);
-
-	// Bind the texture to texture unit 0.
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-
-	// Bind the uniform sampler2D to texture unit 0.
-	gl.uniform1i(frameUniform, 0);
+	let target = gl.TEXTURE_2D;
+	let level = 0;
+	let internalFormat = gl.RGBA;
+	let format = gl.RGBA;
+	let type = gl.UNSIGNED_BYTE;
+	gl.texImage2D(target, level, internalFormat, format, type, video);
 }
 
 function createShader( source, type )
 {
-	var shader = gl.createShader(type);
+	let shader = gl.createShader(type);
 	gl.shaderSource(shader, source);
 	gl.compileShader(shader);
 
-	var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+	let compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
 	if( !compiled )
 	{
-		var log = gl.getShaderInfoLog(shader);
+		let log = gl.getShaderInfoLog(shader);
 		console.log(log);
 		return null;
 	}
@@ -193,8 +184,8 @@ function createProgram()
 {
 	program = gl.createProgram();
 
-	var vertexShader = createShader(vertexSource, gl.VERTEX_SHADER);
-	var fragmentShader = createShader(fragmentSource, gl.FRAGMENT_SHADER);
+	let vertexShader = createShader(vertexSource, gl.VERTEX_SHADER);
+	let fragmentShader = createShader(fragmentSource, gl.FRAGMENT_SHADER);
 
 	gl.attachShader(program, vertexShader);
 	gl.attachShader(program, fragmentShader);
@@ -203,6 +194,19 @@ function createProgram()
 	gl.deleteShader(fragmentShader);
 
 	gl.linkProgram(program);
+}
+
+function useProgram()
+{
+	gl.useProgram(program);
+}
+
+function setUniforms()
+{
+	let uniform = gl.getUniformLocation(program, "u_frame");
+
+	// Set the uniform to the texture unit.
+	gl.uniform1i(uniform, unit);
 }
 
 function onCanPlay( event )
