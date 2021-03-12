9179572ab3e966fd19c6985f267aa20f20ef4a34
adcimon <37084114+adcimon@users.noreply.github.com>
2021-03-12 14:03:38 +0100

    Update index.js

--- mouse-animation/js/index.js
+++ mouse-animation/js/index.js
@@ -4,26 +4,26 @@ window.addEventListener("load", main);
 
 function main()
 {
-    window.addEventListener("mousemove", onMouseMove);
-    document.body.addEventListener("touchmove", onTouchMove);
+	window.addEventListener("mousemove", onMouseMove);
+	document.body.addEventListener("touchmove", onTouchMove);
 }
 
 function onMouseMove( event )
 {
-    let x = event.clientX;
-    let y = event.clientY;
+	let x = event.clientX;
+	let y = event.clientY;
 
-    let particle = new Particle(document.body);
-    particle.spawn(x, y);
+	let particle = new Particle(document.body);
+	particle.spawn(x, y);
 }
 
 function onTouchMove( event )
 {
-    event.preventDefault();
+	event.preventDefault();
 
-    let x = event.touches[0].clientX;
-    let y = event.touches[0].clientY;
+	let x = event.touches[0].clientX;
+	let y = event.touches[0].clientY;
 
-    let particle = new Particle(document.body);
-    particle.spawn(x, y);
+	let particle = new Particle(document.body);
+	particle.spawn(x, y);
 }
