6d7b688d6c9aa99eedc31fffceb00c868f92b0bd
adcimon <37084114+adcimon@users.noreply.github.com>
2021-03-12 17:05:16 +0100

    Update animator.js

--- template/js/animator.js
+++ template/js/animator.js
@@ -8,58 +8,58 @@ function Animator()
 
 Animator.prototype.update = function()
 {
-    let now = window.Date.now();
+	let now = window.Date.now();
 
-    if( this.lastUpdate )
-    {
-        let deltaTime = (now - this.lastUpdate) / 1000; // s
-        this.lastUpdate = now;
+	if( this.lastUpdate )
+	{
+		let deltaTime = (now - this.lastUpdate) / 1000; // s
+		this.lastUpdate = now;
 
-        this.callback(deltaTime);
-    }
-    else
-    {
-        this.lastUpdate = now;
-    }
+		this.callback(deltaTime);
+	}
+	else
+	{
+		this.lastUpdate = now;
+	}
 }
 
 Animator.prototype.tick = function()
 {
-    this.update();
-    this.handle = window.requestAnimationFrame(this.tick.bind(this));
+	this.update();
+	this.handle = window.requestAnimationFrame(this.tick.bind(this));
 }
 
 Animator.prototype.play = function()
 {
-    if( callback && !(callback instanceof Function) )
-    {
-        console.log("Callback " + callback + " is not a function.");
-        return;
-    }
+	if( callback && !(callback instanceof Function) )
+	{
+		console.log("Callback " + callback + " is not a function.");
+		return;
+	}
 
-    if( callback )
-    {
-        this.callback = callback;
-    }
+	if( callback )
+	{
+		this.callback = callback;
+	}
 
-    if( !this.callback )
-    {
-        console.log("Callback is undefined.");
-        return;
-    }
+	if( !this.callback )
+	{
+		console.log("Callback is undefined.");
+		return;
+	}
 
-    if( !this.handle )
-    {
-        this.tick();
-    }
+	if( !this.handle )
+	{
+		this.tick();
+	}
 }
 
 Animator.prototype.stop = function()
 {
-    if( this.handle )
-    {
-        window.cancelAnimationFrame(this.handle);
-        this.handle = undefined;
-        this.lastUpdate = undefined;
-    }
+	if( this.handle )
+	{
+		window.cancelAnimationFrame(this.handle);
+		this.handle = undefined;
+		this.lastUpdate = undefined;
+	}
 }
