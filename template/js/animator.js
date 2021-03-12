a7cbe45b0467661ff331e758bb74fdfdb67ea807
adcimon <37084114+adcimon@users.noreply.github.com>
2021-03-12 17:03:46 +0100

    Add files via upload

---
+++ template/js/animator.js
@@ -0,0 +1,65 @@
+"use strict";
+
+function Animator()
+{
+	this.handle = undefined;
+	this.lastUpdate = undefined;
+}
+
+Animator.prototype.update = function()
+{
+    let now = window.Date.now();
+
+    if( this.lastUpdate )
+    {
+        let deltaTime = (now - this.lastUpdate) / 1000; // s
+        this.lastUpdate = now;
+
+        this.callback(deltaTime);
+    }
+    else
+    {
+        this.lastUpdate = now;
+    }
+}
+
+Animator.prototype.tick = function()
+{
+    this.update();
+    this.handle = window.requestAnimationFrame(this.tick.bind(this));
+}
+
+Animator.prototype.play = function()
+{
+    if( callback && !(callback instanceof Function) )
+    {
+        console.log("Callback " + callback + " is not a function.");
+        return;
+    }
+
+    if( callback )
+    {
+        this.callback = callback;
+    }
+
+    if( !this.callback )
+    {
+        console.log("Callback is undefined.");
+        return;
+    }
+
+    if( !this.handle )
+    {
+        this.tick();
+    }
+}
+
+Animator.prototype.stop = function()
+{
+    if( this.handle )
+    {
+        window.cancelAnimationFrame(this.handle);
+        this.handle = undefined;
+        this.lastUpdate = undefined;
+    }
+}
